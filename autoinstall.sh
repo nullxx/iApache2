a7f49fa952f9b68ee85f43802bb4e5517f61294c
nullx <mail@nullx.me>
2020-08-07 21:22:41 +0200

    autoinstall commented

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,6 +5,7 @@ if [ "${UID}" -ne 0 ]; then
     exit 1
 fi
 
+REPO_URL=https://github.com/nullxx/iApache2/archive/master.tar.gz
 TMP_DIR=/tmp
 
 D_FILE_OUT=iApache2.tar.gz
@@ -16,26 +17,34 @@ TARGET_DIR=/opt/iApache2
 
 SERVICES_DIR=/etc/systemd/system
 
-wget "https://github.com/nullxx/iApache2/archive/master.tar.gz" -O $D_DIR_OUT
+echo "Downloading repository data"
+wget -q -O $D_DIR_OUT "$REPO_URL" 
 mkdir -p $E_DIR_OUT
 tar -zxvf $D_DIR_OUT -C $E_DIR_OUT
 cd $E_DIR_OUT/iApache2-master
+echo "Running npm install at $E_DIR_OUT/iApache2-master"
 npm install
 
+echo "Creating folder $TARGET_DIR"
 mkdir -p $TARGET_DIR
+echo "Moving $E_DIR_OUT/iApache2-master/ to $TARGET_DIR/"
 mv $E_DIR_OUT/iApache2-master/* $TARGET_DIR/.
 
 cd $TARGET_DIR
-ls $TARGET_DIR
+echo "Modifying template service file and saving to $SERVICES_DIR/iApache2.service "
 SERVICE_TEMPLATE=$(cat iApache2.service.template)
 
 echo "${SERVICE_TEMPLATE/<P_PATH>/$TARGET_DIR}" > $SERVICES_DIR/iApache2.service
+echo "Giving 644 permission to $SERVICES_DIR/iApache2.service"
 sudo chmod 644 $SERVICES_DIR/iApache2.service
 
-echo "CLEANING"
+echo "Cleaning temporary directory $D_DIR_OUT"
 rm -rf $D_DIR_OUT
+echo "Cleaning temporary directory $E_DIR_OUT"
 rm -rf $E_DIR_OUT
 
-echo "SETUP SUCCESS"
-
+echo "SUCCESS"
+echo "-------- -------- --------"
 echo "To run iApache: sudo systemctl start iApache2"
+echo "Server at localhost:3000"
+echo "To change PORT create .env file at $TARGET_DIR/.env with 'PORT' var"
